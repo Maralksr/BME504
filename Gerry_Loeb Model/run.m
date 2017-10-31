c9170adbf7e5998d2fdd6fadde96a5ffe4a8528a
Maral <kasiri@usc.edu>
2017-10-31 01:02:47 -0700

    update 3

--- Gerry_Loeb Model/run.m
+++ Gerry_Loeb Model/run.m
@@ -16,10 +16,10 @@ feff_2 = 0;
 
 
 Fs=10000;
-t=0:1/Fs:5;
+t=0:1/Fs:4;
 
 
-Af=0;
+
 L_init=0;
 
 
@@ -46,19 +46,30 @@ Af_fast=zeros(1,length(t)+1);
 F_m=zeros(1,length(t)+1);
 F_t=zeros(1,length(t)+1);
 
-%L=[ones(1,Fs) 0.5*t(Fs+1:2*Fs)+0.5 1.5*ones(1,length(t)-2*Fs)]; 
-%Lse=2-L;
-%V=[zeros(1,Fs) 0.5*ones(1,Fs) zeros(1,length(t)-2*Fs)]; 
+L=1; 
 
-L=[ones(1,2*Fs) 1.5*ones(1,length(t)-2*Fs)]; 
 Lse=2-L;
-V=[zeros(1,2*Fs) 5 zeros(1,length(t)-2*Fs-1)]; 
+
+V=-5;
+
+
+
+
+
+
+%L=[ones(1,1*Fs)  -50*t(Fs+1:Fs+10)+101   0.95*ones(1,1*Fs/2) 50*t(1.5*Fs+11:1.5*Fs+20)-124.1 ...
+    %1.05*ones(1,Fs/2) -50*t(2*Fs+21:2*Fs+30)+151.2  ones(1,length(t)-2*Fs-30) ]; 
+%Lse=2-L;
+%Vd=0.05/(t(Fs+100)-t(Fs));
+%Vu=0.1/(t(1.5*Fs+200)-t(1.5*Fs+100));
+%V=[zeros(1,1*Fs) -Vd*ones(1,10) zeros(1,Fs/2) Vu*ones(1,10) zeros(1,Fs/2) -Vd*ones(1,10) zeros(1,length(t)-2*Fs-30)]; 
 
 
 
 Ueff_dot=zeros(1,length(t)+1);
+amp=0.3;
 Ueff=zeros(1,length(t)+1);
-U=zeros(1,length(t)+1);
+U=[zeros(1,1*Fs) (amp)*1/2*t(1:2*Fs) amp*ones(1,length(t)-3*Fs)];
 for i=1:length(t)
     
  %active force
@@ -71,17 +82,17 @@ for i=1:length(t)
 Ueff_dot(i)=(U(i)-Ueff(i))/Tu;
 Ueff(i+1)=Ueff_dot(i)*1/Fs+U(i);
  
-[Leff_s]= L_eff_fcn(Fs,L(i) ,Af_slow(i), L_eff_s(i));
+[Leff_s]= L_eff_fcn(Fs,L ,Af_slow(i), L_eff_s(i));
 L_eff_s(i+1)=Leff_s;
 
-[Leff_f]= L_eff_fcn(Fs,L(i) ,Af_fast(i), L_eff_f(i));
+[Leff_f]= L_eff_fcn(Fs,L ,Af_fast(i), L_eff_f(i));
 L_eff_f(i+1)=Leff_f;
  
 [S]=Sag_fcn(Fs, f_eff_slow(i), S(i));
 S(i+1)=S;
 
-[f_eff_slow, f_eff_dot_slow ,f_int_slow, f_env_slow]= f_eff_fcn(Fs, f_int_slow(i), L(i), f_env_slow(i), 'slow', f_eff_dot_slow(i), f_eff_slow(i), Ueff(i), Af_slow(i));
-[f_eff_fast, f_eff_dot_fast ,f_int_fast, f_env_fast]= f_eff_fcn(Fs, f_int_fast(i), L(i), f_env_fast(i), 'fast', f_eff_dot_fast(i), f_eff_fast(i), Ueff(i), Af_fast(i));
+[f_eff_slow, f_eff_dot_slow ,f_int_slow, f_env_slow]= f_eff_fcn(Fs, f_int_slow(i), L, f_env_slow(i), 'slow', f_eff_dot_slow(i), f_eff_slow(i), Ueff(i), Af_slow(i));
+[f_eff_fast, f_eff_dot_fast ,f_int_fast, f_env_fast]= f_eff_fcn(Fs, f_int_fast(i), L, f_env_fast(i), 'fast', f_eff_dot_fast(i), f_eff_fast(i), Ueff(i), Af_fast(i));
 
 f_int_slow(i+1)=f_int_slow;
 f_int_fast(i+1)=f_int_fast;
@@ -95,7 +106,7 @@ f_env_fast(i+1)=f_env_fast;
 
 
 
-[Y]= Yield_fnc(Fs, V(i), Y(i));
+[Y]= Yield_fnc(Fs, V, Y(i));
 Y(i+1)=Y;
 
 [Af_slow, Af_fast]=activ_force(Y(i),S(i),L_eff_s(i),L_eff_f(i), f_eff_slow(i), f_eff_fast(i));
@@ -103,15 +114,15 @@ Af_slow(i+1)=Af_slow;
 Af_fast(i+1)=Af_fast;
 
 %force length
-FL_f=FL_fast(L(i));
-FL_s=FL_slow(L(i));
+FL_f=FL_fast(L);
+FL_s=FL_slow(L);
 
-if V(i)<=0
-    FV_s=FVcon_slow(L(i),V(i));
-    FV_f=FVcon_fast(L(i),V(i));
+if V<=0
+    FV_s=FVcon_slow(L,V);
+    FV_f=FVcon_fast(L,V);
 else
-    FV_s=FVecc_slow(L(i),V(i));
-    FV_f=FVecc_fast(L(i),V(i));
+    FV_s=FVecc_slow(L,V);
+    FV_f=FVecc_fast(L,V);
 end
 
 Ur = 0.8; % activation level at which all the motor units are recruited (Song et al. 2008)
@@ -127,8 +138,8 @@ dif_U_F = Ueff(i) - UF_th; % difference between effective neural drive and thres
 W1 = dif_U_S/(dif_U_S+dif_U_F); % proportion of active slow-twitch fiber of total active muscle (0-1)
 W2 = dif_U_F/(dif_U_S+dif_U_F); % proportion of active fast-twitch fiber of total active muscle (0-1)
 
-FPE1=Fpe1(L(i),V(i)); %Stretching contractile passive element (fascicle) force (F0)
-FPE2=Fpe2(L(i));
+FPE1=Fpe1(L,V); %Stretching contractile passive element (fascicle) force (F0)
+FPE2=Fpe2(L);
 
 %----------
 %total force
@@ -142,13 +153,39 @@ F_muscle = Fce(1) + FPE1;
 
 
 %tendon force
-F_tendon=Fse(Lse(i));
+F_tendon=Fse(Lse);
 
 
 F_m(i+1)=F_muscle;
 F_t(i+1)=F_tendon;
 MT_force(i+1)=F_muscle+ F_tendon;
 
-i
+
 
 end
+
+
+figure(1)
+plot(t,MT_force(2:length(t)+1))
+title('MT')
+
+figure(2)
+plot(t,F_m(2:length(t)+1))
+title('M')
+
+figure(3)
+plot(t,F_t(2:length(t)+1))
+title('T')
+
+figure(4)
+plot(t,L)
+title('L_m')
+
+figure(5)
+plot(t,Lse)
+title('L_t')
+
+figure(6)
+plot(t,L+Lse)
+title('L_mt')
+
---
+++ Gerry_Loeb Model/testtset.m
@@ -0,0 +1,4 @@
+for i=1:length(L)+1
+    dl(i)=(L(i+1)-L(i))/L(i);
+    df(i)=(F_m(i+1)-F_m(i)/F_m(i));
+end
