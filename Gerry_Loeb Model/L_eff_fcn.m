ae996a1c0bb134cb73274db2561af9b95ddfe323
Maral <kasiri@usc.edu>
2017-11-26 17:37:59 -0800

    final

--- Gerry_Loeb Model/L_eff_fcn.m
+++
@@ -1,9 +0,0 @@
-
-function [Leff]= L_eff_fcn(Fs ,L, Af, L_eff0)
-
-T_L= 0.088; %ms
-
-L_eff_dot= (L-L_eff0)^3/T_L/(1-Af);
-Leff= L_eff_dot*1/Fs +L_eff0;
-
-end
--- Gerry_Loeb Model/testtset.m
+++
@@ -1,4 +0,0 @@
-for i=1:length(L)+1
-    dl(i)=(L(i+1)-L(i))/L(i);
-    df(i)=(F_m(i+1)-F_m(i)/F_m(i));
-end
